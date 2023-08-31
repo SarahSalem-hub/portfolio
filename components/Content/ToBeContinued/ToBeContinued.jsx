2e921914f63b3d4b25f40e57af36b06b04865959
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2023-08-31 20:24:52 +0300

    edit conclusion msg in home page

--- components/Content/ToBeContinued/ToBeContinued.jsx
+++ components/Content/ToBeContinued/ToBeContinued.jsx
@@ -8,7 +8,7 @@ function ToBeContinued() {
              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffc68f" fill-opacity="0.4" d="M0,256L34.3,256C68.6,256,137,256,206,245.3C274.3,235,343,213,411,186.7C480,160,549,128,617,133.3C685.7,139,754,181,823,170.7C891.4,160,960,96,1029,96C1097.1,96,1166,160,1234,176C1302.9,192,1371,160,1406,144L1440,128L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
         {/* </ShadowDiv> */}
       <ToBeText>The Journey never ends..</ToBeText>
-      <ToBeText>soon this page will be be filled !</ToBeText>
+      <ToBeText>soon this page will be filled !</ToBeText>
     </TobeContDiv>
   );
 }
